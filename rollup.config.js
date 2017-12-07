08e359c9d3dafe11a8d754c557a0b44f3e400b19
Geeku <ggongpei@gmail.com>
2017-12-07 13:25:39 +0800

    uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,28 @@
 import typescript from 'rollup-plugin-typescript2';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
-	input: './src/jsocr.ts',
+const srcPath = './src/jsocr.ts';
+const name = 'JsOCR';
+
+export default [{
+	input: srcPath,
 	output: {
-		name: 'JsOCR',
+		name: name,
 		file: './dist/jsocr.js',
 		format: 'umd'
 	},
 	plugins: [
         typescript()
 	]
-}
+},{
+	input: srcPath,
+	output: {
+		name: name,
+		file: './dist/jsocr.min.js',
+		format: 'umd'
+	},
+	plugins: [
+        typescript(),
+        uglify()
+	]
+}]
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,15 +1,17 @@
 import typescript from 'rollup-plugin-typescript2';
 import sass from 'rollup-plugin-sass';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
-	input: './example/example.ts',
-	output: {
+    input: './example/example.ts',
+    output: {
         name: 'JsOCR',
         file: './example/example.js',
         format: 'umd'
-	},
-	plugins: [
+    },
+    plugins: [
         sass({ output: 'example/example.css' }),
-        typescript()
-	]
+        typescript(),
+        uglify()
+    ]
 }
